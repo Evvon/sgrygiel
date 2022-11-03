f3b458fffca368e732419af3327889c2dbbda3c5
Wojciech <wojcikv97@gmail.com>
2022-11-03 10:52:56 +0100

    add media for mobile devices - not tablets yet

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,17 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { menuData } from "../data/menuData";
 
+const handleClick = () => {
+  const menu = document.querySelector(".menu__list");
+  menu.style.top = "25px";
+  menu.style.left = "100%";
+};
+
 const Menu = () => {
   return (
     <nav className="menu">
       <ul className="menu__list">
         {menuData.map(({ link, name, id }) => (
           <li className="menu__element" key={id}>
-            <Link to={link} className="menu__link">
+            <Link onClick={handleClick} to={link} className="menu__link">
               {name}
             </Link>
           </li>
         ))}
+        <i onClick={handleClick} className="menu__close fa-solid fa-xmark"></i>
       </ul>
     </nav>
   );
